c0663ca6c0ba26dbe589243d65e84b98db794ddf
Simon Ektjärn <debarquer@gmail.com>
2024-10-14 13:33:15 +0200

    Add Bish Bosh functionality and UI
    This commit adds the functionality and user interface for the Bish Bosh game. The game allows users to input a length, Bish number, and Bosh number, and then generates a list of numbers with corresponding labels of "Bish", "Bosh", or "Bish Bosh" based on divisibility. The game logic is implemented in the script.js file, and the UI elements are added to the index.html file.

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+function bishBosh() {
+    var length = document.getElementById("length").value;
+    var bish = document.getElementById("bish").value;
+    var bosh = document.getElementById("bosh").value;
+
+    if(length <= 0 || bish < 0 || bosh < 0) return;
+
+    let output = "Result:<br><ol>";
+    for (let i = 1; i <= length; i++) {
+
+        if(i % bish === 0 && i % bosh === 0) {
+            output += "<li>" + "BishBosh" + "</li>";
+        }
+        else if (i % bish === 0) {
+            output += "<li>" + "Bish" + "</li>";
+        }
+        else if (i % bosh === 0) {
+            output += "<li>" + "Bosh" + "</li>";
+        }
+        else {
+            output += "<li>" + i + "</li>";
+        }    
+    }
+    output += "</ol>";
+    var r = document.getElementById("result");
+    r.innerHTML = output;
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("bishboshbutton").addEventListener("click", bishBosh);
+});
